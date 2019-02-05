61415c1991770b1d5b4e1c8e18212548e46abeb6
Brad Newton <bradnew10@gmail.com>
2019-02-04 22:28:34 -0800

    changed URL to a functional download link

--- app/server.py
+++ app/server.py
@@ -8,7 +8,7 @@ from io import BytesIO
 from fastai import *
 from fastai.vision import *
 
-export_file_url = 'https://drive.google.com/uc?export=download&id=ttps://drive.google.com/open?id=1oPkV-QSWHjqtLoNTcxzHvHWFYgJwcEP'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1oPkV-QSWHjqtLoNTcxzHvHWFYgJwcEPJ'
 export_file_name = 'export.pkl'
 
 classes = ['Zebra', 'Horse', 'Donkey']
